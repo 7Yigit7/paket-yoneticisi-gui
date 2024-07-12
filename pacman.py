e52ead12c1c533e5aa14166be81ca367db82f6d9
7Yigit7 <yigitcitak.32@gmail.com>
2024-07-13 00:55:24 +0300

    Add files via upload

---
+++ pacman.py
@@ -0,0 +1,82 @@
+#Yazan yiğit
+
+import tkinter as tk
+from os import system
+ekran = tk.Tk()
+ekran.maxsize(500,700)
+ekran.minsize(500,700)
+ekran.title("PYGUI - pacman")
+
+def install():
+    ins = paket.get()
+    system(f"sudo pacman -S {ins}")
+def upgrade():
+    system("sudo pacman -su")
+def update():
+    system("sudo pacman -sy")
+
+title2 = tk.Label(
+    ekran,
+    text="Paket Yöneticisi GUI",
+    font="italic 25",
+    fg="blue",
+    ).pack()
+
+os = tk.Label(
+    ekran,
+    text="Pacman\n",
+    font="italic 25",
+    fg="blue",
+    ).pack()
+
+title3 = tk.Label(
+    ekran,
+    text="kuruluacak paketler:",
+    font="italic 15",
+    ).pack()
+
+paket = tk.Entry(ekran)
+paket.pack()
+
+paket_b = tk.Button(
+    ekran,
+    text="Kur",
+    font="italic 15",
+    command=install
+    ).pack()
+
+bos32 = tk.Label(
+    ekran,
+    text="",
+    font="italic 25",
+    ).pack()
+
+upgrade_b = tk.Button(
+    ekran,
+    text="Upgrade",
+    font="italic 20",
+    fg="blue",
+    command=upgrade,
+    ).pack()
+
+bos3 = tk.Label(
+    ekran,
+    text="",
+    font="italic 25",
+    ).pack()
+
+update_b = tk.Button(
+    ekran,
+    text="Update",
+    font="italic 20",
+    fg="blue",
+    command=update,
+    ).pack()
+
+imza = tk.Label(
+    ekran,
+    text="\nHazırlayan Yiğit",
+    font="italic 25",
+    ).pack()
+
+ekran.mainloop()
